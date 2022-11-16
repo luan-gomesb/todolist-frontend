dcb1871ae83ff7d64daa9056045a501b63db116e
luan-gomesb <luan.gomesb@live.com>
2022-11-16 10:35:53 -0300

    make useTodoListHook receive todo as dependency

--- src/presenters/useTodoListHook.ts
+++ src/presenters/useTodoListHook.ts
@@ -1,29 +1,30 @@
 import { useReducer, useRef, useState } from 'react';
 import TodoList, { Item } from '../entities/Todolist';
+import TodoListObserver from '../TodoListObserver';
 
 export type TodoActions = 'create' | 'toggle' | 'remove';
 export type TodolistDispatch = (action: TodoActions, payload: any) => void;
 export type useTodoListResponse = [Item[], TodolistDispatch];
 
 export default function useTodoListHook(
-  initialTodos: Item[],
+  todolist: TodoList,
 ): useTodoListResponse {
-  const todolist = useRef(new TodoList(initialTodos));
-  const [todos, setTodos] = useState<Item[]>([...todolist.current.items]);
+  // const todolist = useRef(initialTodoList);
+  const [todos, setTodos] = useState<Item[]>([...todolist.items]);
 
   const dispatch = (action: TodoActions, payload: any) => {
     switch (action) {
       case 'create':
-        todolist.current.create(payload.description);
-        setTodos([...todolist.current.items]);
+        todolist.create(payload.description);
+        setTodos([...todolist.items]);
         break;
       case 'toggle':
-        todolist.current.toggle(payload.id);
-        setTodos([...todolist.current.items]);
+        todolist.toggle(payload.id);
+        setTodos([...todolist.items]);
         break;
       case 'remove':
-        todolist.current.remove(payload.id);
-        setTodos([...todolist.current.items]);
+        todolist.remove(payload.id);
+        setTodos([...todolist.items]);
         break;
     }
   };
